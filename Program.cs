b691580acd45ff078db136755ce5eb3782591e27
jslucascba <45663491+jslucascba@users.noreply.github.com>
2020-10-16 14:42:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fall_2020_starter_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# fall2020-starter-code" 
